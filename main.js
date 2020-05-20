61fc39290d27e8e720f4de2c60c0475e4563ad2d
Mantas Brasiunas <mantas.brasiunas@totalren.com>
2020-05-20 12:15:49 +0100

    input

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ function example1() {
     alert("This is the second alert.");
 }
 document.getElementById("demo").innerHTML = Date();
+function checkRange() {
+    var display = "none";
+    var value = document.getElementById("price1").value;
+    var paragraphList = document.getElementsByTagName("p");
+    var firstParagraph = paragraphList[1];
+    if (value >= 80) {
+        display = "block";
+    }
+    firstParagraph.style.display = display;
+}
--- main.ts
+++ main.ts
@@ -4,3 +4,14 @@ function example1() {
 }
 
 document.getElementById("demo").innerHTML = Date();
+
+function checkRange() {
+  var display = "none";
+  var value = document.getElementById("price1").value;
+  var paragraphList = document.getElementsByTagName("p");
+  var firstParagraph = paragraphList[1];
+  if (value >= 80) {
+    display = "block";
+  }
+  firstParagraph.style.display = display;
+}
